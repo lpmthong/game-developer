3e7bfaed50979e591ddccc33fe7b7319b729d55e
lpmthong.dev@gmail.com <lpmthong.dev@gmail.com@051436d1-a6fc-8618-3212-06cce84ee763>
2013-10-08 15:11:03 +0000

    lam dc mot phan MapEditor

---
+++ trunk/BestMario/source code/MapEditor/MapEditor/MapEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace MapEditor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var t = Task.Factory.StartNew(() => Application.EnableVisualStyles());
+            var t1 = Task.Factory.StartNew(() => Application.SetCompatibleTextRenderingDefault(false));
+            var t2 = Task.Factory.StartNew(() => Application.Run(new frmMain()));
+            t2.Wait();
+        }
+    }
+}
